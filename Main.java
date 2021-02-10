7781ebc9f579b4f12bcb5ec4685e06feb0f6ed8b
niall2000 <78852841+niall2000@users.noreply.github.com>
2021-02-10 20:03:07 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,6 +13,7 @@ public class Main {
         Model model = new Model();               // Create shared database
         Controller1 c1 = new Controller1(model); // Create first controller
         Controller2 c2 = new Controller2(model); // Create second controller
+        Controller2 c3 = new Controller2(model); //create third controller
         
     } // main
   
